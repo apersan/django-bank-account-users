4f78beb68e413a7da5ed0340892a88553935c45a
apersan <alejandro@houzz.com>
2019-10-12 11:23:50 +0200

    Added an app to bulk import and export data for the crud app (extra)

--- web/crud/admin.py
+++ web/crud/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from .models import User
 
-# Register your models here.
+
+# @admin.register(User)
+class ViewAdmin(ImportExportModelAdmin):
+    exclude = ('id', )
--- web/user_management_system/settings.py
+++ web/user_management_system/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     # apps
     'crud',
     'localflavor',
+    'import_export',
 ]
 
 MIDDLEWARE = [
