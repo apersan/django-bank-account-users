20f93921eb393a3ccef225408417eae3dabdd7f0
apersan <alejandro@houzz.com>
2019-10-12 10:52:14 +0200

    Created models and registered localflavor app in settings

--- web/crud/models.py
+++ web/crud/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from localflavor.generic.models import IBANField
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import User as AdminUser
 
-# Create your models here.
+
+class User(models.Model):
+
+    nameRegex = RegexValidator(
+        regex=r'^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.\'-]+$',
+        message='Only characters from A to Z are allowed.'
+    )
+
+    creator = models.ForeignKey(AdminUser, on_delete=models.CASCADE)
+
+    firstName = models.CharField(
+        max_length=35,
+        verbose_name='First Name',
+        help_text='First Name',
+        validators=[nameRegex],
+        blank=False
+    )
+
+    lastName = models.CharField(
+        max_length=50,
+        verbose_name='Last Name',
+        help_text="Last Name",
+        validators=[nameRegex],
+        blank=False
+    )
+
+    iban = IBANField(unique=True)
+
+    def __str__(self):
+        return 'First Name: {0}, Last Name: {1}'.format(self.firstName, self.lastName)
